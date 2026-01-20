82fb1a3d5330d1309ed67ec02f2d946e12b410e4
Sai Rama Doddi <sairama1026@gmail.com>
2026-01-20 09:50:42 +0530

    Added links in frintend

--- Frontend/src/api/inventoryApi.ts
+++ Frontend/src/api/inventoryApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const inventoryApi = axios.create({
-  baseURL: import.meta.env.VITE_INVENTORY_API || "http://localhost:8050/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8050/api",
   timeout: 10000,
 });
 
--- Frontend/src/api/offlineInventoryApi.ts
+++ Frontend/src/api/offlineInventoryApi.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const offlineInventoryApi = axios.create({
-  baseURL: "http://localhost:8050/api/admin",
+  baseURL: import.meta.env.VITE_API_BASE_URL||"http://localhost:8050/api/admin",
 });
 
 export default offlineInventoryApi;
