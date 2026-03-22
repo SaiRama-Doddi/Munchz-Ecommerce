571638ccfc0f2f0f865a87e552176fedb15ec599
Pandu1712a <prasannakumarpanda2000@gmail.com>
2026-03-22 21:22:50 +0530

    cart page

--- Frontend/src/pages/UserOrders.tsx
+++ Frontend/src/pages/UserOrders.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { Mail, User, LayoutGrid, List, Eye, Upload, Star, Trash2, ShoppingBag } from "lucide-react";
+import { Mail, User, LayoutGrid, List, Eye, Upload, Star, Trash2, ShoppingBag, X } from "lucide-react";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
 import { FaTimes } from "react-icons/fa";
