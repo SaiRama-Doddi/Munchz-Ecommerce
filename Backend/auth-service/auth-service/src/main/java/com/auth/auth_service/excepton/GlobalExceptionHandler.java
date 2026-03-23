b70f94dc929529d16f219ed8272a66e550adc1ae
Pandu1712a <prasannakumarpanda2000@gmail.com>
2026-03-23 21:04:26 +0530

    Fix missing import for HttpStatus

--- Backend/auth-service/auth-service/src/main/java/com/auth/auth_service/excepton/GlobalExceptionHandler.java
+++ Backend/auth-service/auth-service/src/main/java/com/auth/auth_service/excepton/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.auth.auth_service.excepton;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
