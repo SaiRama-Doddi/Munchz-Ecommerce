2f16d3ce193f55d4fa9de82f0ce6e81091c15e8e
Pandu1712a <prasannakumarpanda2000@gmail.com>
2026-04-02 17:42:46 +0530

    Frontend erroer

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App";
 import ScrollToTop from "./components/ScrollToTop"; // ✅ ADD THIS
@@ -24,6 +25,7 @@ createRoot(document.getElementById("root")!).render(
           <AuthProvider>
             <CartProvider>
               <App />
+              <Toaster position="top-right" />
             </CartProvider>
           </AuthProvider>
         </BrowserRouter>
